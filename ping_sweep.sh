f4b9c1dcfac27a0528f301d34521b6b88812276a
Gabriel Ribeiro <84105921+GabrielRibbeiro@users.noreply.github.com>
2024-11-23 15:43:00 -0300

    Update ping_sweep.sh
    Adicionado correção e  logs

--- ping_sweep.sh
+++ ping_sweep.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Definir o endereço da rede, definia a  faixa de  rede .
-network="172.30.0"
+read -p "Informe a rede: " rede
 
-# Função de ping individual
-ping_sweep() {
-    ip=$1
-    # Usando o parâmetro -W para definir o timeout de espera do ping
-    ping -c 1 -W 1 ${network}.$ip | grep "64 bytes" & # Coloca o ping em paralelo
+if [[ -z "$rede" ]];then
+    echo -e "\e[033m Erro: Não foi informado nenhuma rede \n Execute  o scritp novamente e  informe o parâmetro novamente \e[0m"
+    echo -e "\e[033m Exemplo: $0 192.168.0 \e[0m"
+    exit 1
+fi
+echo -e "\e[033m Realizando Ping Sweep na rede $rede \e[0m"
+
+
+ping_sweep(){
+    ping -c 1 -W 1 ${rede}.$ip | grep "64 bytes" | cut -d ":" -f 1 | cut -d " " -f 4 | tee -a host_ativos.txt &
 }
 
-# Executando o ping sweep para os IPs de 1 a 254
-for ip in $(seq 1 254); do
-    ping_sweep $ip
+echo -e "\e[033mHosts encontrados: \e[0m"
+for ip in $(seq 1 254);do
+    ping_sweep
 done
-
-# Espera todos os pings terminarem
 wait
